3d040a80895f89f3d92a3dba180db8bfadc60aaf
art-ste-web <art.ste.web@gmail.com>
2020-10-24 20:58:24 +0300

    dev

--- src/Table.js
+++ src/Table.js
@@ -12,28 +12,33 @@ const TableHeader = () => {
 }
 
 const TableBody = (props) => {
+    console.log(props);
     const rows = props.characterData.map((row, index) => {
         return (
             <tr key = {index}>
                 <td>{ row.name }</td>
                 <td>{ row.job }</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
             </tr>
         )
     })
-return <tbody>{ rows }</ tbody>;
+    return <tbody>{ rows }</ tbody>;
     
 }
 
-class Table extends Component {
-    render() {
-        const {characterData} = this.props;
-        return (
-            <table>
-                <TableHeader />
-                <TableBody characterData = { characterData } />
-            </table>
-        )
-    }
+
+
+const Table = (props) => {
+    const {characterData, removeCharacter} = props
+
+    return (
+        <table>
+            <TableHeader />
+            <TableBody characterData = {characterData} removeCharacter = {removeCharacter} />
+        </table>
+    )
 }
 
 export default Table;
