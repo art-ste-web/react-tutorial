c3ddd1840138d34edfe3365e7f18ebe4712eb7a9
art-ste-web <art.ste.web@gmail.com>
2020-10-30 19:58:12 +0200

    Updates

---
+++ precache-manifest.1a048300db2fc25a839c129068d6fb79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f1a913ef2ecdf962f331106a8c06d8",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "509681c72c6e1bde0198",
+    "url": "/react-tutorial/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509681c72c6e1bde0198",
+    "url": "/react-tutorial/static/js/main.870a53a6.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
